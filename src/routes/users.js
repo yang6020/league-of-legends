ac4316d9b18e4d38ad425fc55cf65c83de6adeb8
Jacqueline Yang <justinvyang@gmail.com>
2019-06-08 20:06:53 -0700

    add default user route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,12 @@ const router = express.Router()
 const requestify = require('requestify')
 const config = require('../../config.json')
 
+// Send false if there is no user queried (we will not get every single league user ktnx)
+router.get('/', (req, res) => {
+  res.status(404)
+  res.send('Not Found')
+})
+
 // Gets details of a specific league player using their username
 router.get('/:username', (req, res) => {
   const url = `
@@ -11,8 +17,14 @@ router.get('/:username', (req, res) => {
       req.params.username
     }?api_key=${config.APIKEY}
     `
+  console.log(url)
   requestify.get(url).then(response => {
-    res.send(response.getBody())
+    if (response) {
+      res.send(response.getBody())
+    } else {
+      res.status(404)
+      res.send('Not Found')
+    }
   })
 })
 
