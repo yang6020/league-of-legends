3deb25f2b2f7582f4da3289d7ca8116571dd880e
Jacqueline Yang <justinvyang@gmail.com>
2019-06-08 17:55:59 -0700

    add user controller + /:username route and functionality. This gets details of a league user.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
+// Imports
 const express = require('express')
+const bodyParser = require('body-parser')
 const config = require('../config.json')
+const users = require('./routes/users')
 
+// Server setup
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/ping', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'Connection successful'
-  })
-})
-
+// Env Variables
 const Port = config.PORT
+
+// Routes
+app.use('/users', users)
 
 app.listen(Port, () => {
   console.log(`server running on port ${Port}`)
---
+++ src/routes/users.js
@@ -0,0 +1,19 @@
+// Import variables
+const express = require('express')
+const router = express.Router()
+const requestify = require('requestify')
+const config = require('../../config.json')
+
+// Gets details of a specific league player using their username
+router.get('/:username', (req, res) => {
+  const url = `
+    https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${
+      req.params.username
+    }?api_key=${config.APIKEY}
+    `
+  requestify.get(url).then(function(response) {
+    res.send(response.getBody())
+  })
+})
+
+module.exports = router
